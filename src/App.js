5d19b741b69e38832c0a4f69b9de58ce3494ae43
Querensales <querenhsales@gmail.com>
2022-02-21 21:14:30 -0300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>QuerenSales.Dev</h3>
+            </div>
+            <div className="item-menu">
+              <a href="#">Login</a>
+            </div>
+          </div>
+
+          <div className="form">
+            <h2>Entre em Contato!</h2>
+            <form>
+              <div className="items-form">
+                <input placeholder="Nome" type="text" />
+                <input placeholder="Sobrenome" type="text" />
+                <input placeholder="(xx) xxxxx-xxxx" type="text" />
+                <input placeholder="Email" type="text" />
+                <input type="submit" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="conteudo">
+        <div className="center">
+          <div className="conteudo-single">
+            <h3>Landing page</h3>
+            <p>Desenvolvida em react! Para adquirir conhecimento.</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
